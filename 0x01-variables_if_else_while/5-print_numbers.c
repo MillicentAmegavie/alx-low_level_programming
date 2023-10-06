2e32917cf0f2906cda6429a1b711680c4a2f4bb0
Millicent Amegavie <miillicent.amegavie@yahoo.com>
2023-10-06 08:42:04 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point
+ *
+ * Print all single digit numbers of base 10 starting from 0.
+ *
+ * Return: 0 on Success.
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+		printf("%d", m);
+	printf("\n");
+	return (0);
+}
