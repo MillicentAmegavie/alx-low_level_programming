9375968894da562e278f8d95d7f60199b993f8a1
Millicent Amegavie <miillicent.amegavie@yahoo.com>
2023-11-06 03:53:30 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point.
+ * Description - The program that prints the name of the file.
+ * Return: Always 0 on Success.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
