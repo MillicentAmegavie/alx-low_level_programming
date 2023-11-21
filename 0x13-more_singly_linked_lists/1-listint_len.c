c6a8cd45c95b7f8da75db79181f1e5215533f149
Millicent Amegavie <miillicent.amegavie@yahoo.com>
2023-11-21 01:23:08 -0800

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - The Entry Point. 
+ * Description - Returns the number of elements in
+ * a linked list.
+ * @h: A pointer to the head of a list.
+ * Return: numbers of nodes.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nn = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nn++;
+	}
+	return (nn);
+}
